397fb5ed6b5b1b91f0384b0f510ef4dc7f650a8b
Anne Saber <iarabejo3@gmail.com>
2022-05-09 21:03:56 -0500

    Renamed component files to jsx and included navbar with links.

---
+++ src/projects.js
@@ -0,0 +1,75 @@
+// export const projects = [
+  
+//   {
+//     id: 1,
+//         name: 'Nerds Meet Nerds',
+//         src: './Assets/images/nerds-meet-nerds.jpg',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+  
+//   {
+//     id: 2,
+//         name: 'Nerds Meet Nerds',
+//         src: './img/quiz.PNG',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+  
+//   {
+//     id: 3,
+//         name: 'Nerds Meet Nerds',
+//         src: './img/quiz.PNG',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+  
+//   {
+//     id: 4,
+//         name: 'Nerds Meet Nerds',
+//         src: './img/quiz.PNG',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+  
+//   {
+//     id: 5,
+//         name: 'Nerds Meet Nerds',
+//         src: './img/quiz.PNG',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+  
+//   {
+//     id: 6,
+//         name: 'Nerds Meet Nerds',
+//         src: './img/quiz.PNG',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+  
+//   {
+//     id: 7,
+//         name: 'Nerds Meet Nerds',
+//         src: './img/quiz.PNG',
+//         desc: 'A timed code quiz on JavaScript fundamentals.',
+//         live: 'https://nvsco-10.github.io/timed-code-quiz/',
+//         repo: 'https://github.com/nvsco-10/timed-code-quiz',
+//         tech: ['HTML', 'CSS', 'JavaScript']
+//   },
+
+  
+
+// ]
