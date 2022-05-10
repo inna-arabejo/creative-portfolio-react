8a9f92426e0881daefa7bd47ff6e2376311dc187
Anne Saber <iarabejo3@gmail.com>
2022-05-10 03:13:42 -0500

    Converted svg images into png in icons folder.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 let ProfilePic = require ("../images/profile-pic.JPG");
+let ReactIcon = require ("../images/icons/Reacticon.svg.png");
+let NodeJS = require ("../images/icons/nodejs.png");
+let JavaScript = require ("../images/icons/js.png");
+let CSSIcon = require ("../images/icons/css.png");
+let HTMLIcon = require ("../images/icons/html.png");
+let bootstrapIcon = require ("../images/icons/bootstraplogo.png");
+let mongoDB = require ("../images/icons/mongodbicon.png");
+let ExpressJS = require ("../images/icons/expressicon.png");
+let VSCode = require ("../images/icons/vscode.png");
+let Insomnia = require ("../images/icons/insomniaicon.png");
+let heroku = require ("../images/icons/heroku.png");
+let gitHub = require ("../images/icons/github.png");
+let StackOverFlow = require ("../images/icons/stackoverflow.png");
+let trello = require ("../images/icons/trello.png");
 
 function About() {
   return (
@@ -21,6 +35,76 @@ function About() {
               To me, web development isn't just about building functional code and delivering aesthetically pleasing web pages, but also how accessable it is. It's about building an application that improves access to information so it is easier to navigate and connect users to the services they need.
             </p>
           </div>
+
+          {/* My Skills */}
+          <section id="skills-tools">
+            <div class="my-skills">
+              <h3 id="skill-font">My Skills</h3>
+              <div class="row">
+                <div class="col">
+                  <img src={ReactIcon} alt = "T" width = "100" height = "100" />
+                  <h5>React</h5>
+                </div>
+                <div class="col">
+                  <img src={NodeJS} alt = "Te" width = "150" height = "100" />
+                  <h5>Node.js</h5>
+                </div>
+                <div class="col">
+                  <img src={JavaScript} alt = "Tes" width = "100" height = "100" />
+                  <h5>JavaScript</h5>
+                </div>
+                <div class="col">
+                  <img src={CSSIcon} alt = "Test" width = "80" height = "100" />
+                  <h5>CSS</h5>
+                </div>
+                <div class="col">
+                  <img src={HTMLIcon} alt = "Test I" width = "100" height = "100" />
+                  <h5>HTML</h5>
+                </div>
+                <div class="col">
+                  <img src={bootstrapIcon} alt = "Test Im" width = "100" height = "100" />
+                  <h5>Bootstrap</h5>
+                </div>
+                <div class="col">
+                  <img src={mongoDB} alt = "Test Ima" width = "100" height = "100" />
+                  <h5>MongoDB</h5>
+                </div>
+                <div class="col">
+                  <img src={ExpressJS} alt = "Test Imag" width = "100" height = "100" />
+                  <h5>Express.js</h5>
+                </div>
+              </div>
+            </div>
+
+            <div class="tools">
+              <div class="row">
+                <div class="col">
+                  <img src={VSCode} alt = "Test Image" width = "100" height = "100" />
+                  <h5>Visual Studio Code</h5>
+                </div>
+                <div class="col">
+                  <img src={Insomnia} alt = "Test Image 2" width = "100" height = "100" />
+                  <h5>Insomnia</h5>
+                </div>
+                <div class="col">
+                  <img src={heroku} alt = "Test Image 3" width = "150" height = "100" />
+                  <h5>Heroku</h5>
+                </div>
+                <div class="col">
+                  <img src={gitHub} alt = "Test Image 4" width = "150" height = "100" />
+                  <h5>GitHub</h5>
+                </div>
+                <div class="col">
+                  <img src={StackOverFlow} alt = "Test Image 5" width = "150" height = "100" />
+                  <h5>Stack Overflow</h5>
+                </div>
+                <div class="col">
+                  <img src={trello} alt = "Test Image 6" width = "150" height = "100" />
+                  <h5>Trello</h5>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
