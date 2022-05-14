a7ebe2e4c2b430b7302dba14e2fe248265804f7c
Anne Saber <iarabejo3@gmail.com>
2022-05-13 22:56:16 -0500

    Testing homepage links and troubleshooting gh-pages.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,34 +6,34 @@ function Navigation() {
     <div className="navigation">
       <nav className="navbar navbar-expand fixed-top">
         <div className="container">
-          <NavLink className="navbar-brand" to="/home">
+          <NavLink className="navbar-brand" to="/creative-portfolio-react/home">
             Inna Arabejo
           </NavLink>
           <div>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/home">
+                <NavLink className="nav-link" to="/creative-portfolio-react/home">
                   Home
                   <span className="sr-only">(current)</span>
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
+                <NavLink className="nav-link" to="/creative-portfolio-react/about">
                   About
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/projects">
+                <NavLink className="nav-link" to="/creative-portfolio-react/projects">
                   Projects
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
+                <NavLink className="nav-link" to="/creative-portfolio-react/contact">
                   Contact
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/resume">
+                <NavLink className="nav-link" to="/creative-portfolio-react/resume">
                   Resume
                 </NavLink>
               </li>
