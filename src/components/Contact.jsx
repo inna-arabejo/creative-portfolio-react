a260fed5cefb6e74889f3a61480feb841336e112
Anne Saber <iarabejo3@gmail.com>
2022-05-15 15:02:46 -0500

    Successfully implemented contact form for users to send me a message.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-function Contact() {
+export const Contact = () => {
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_v8bo3k6', 'template_r58y7ci', form.current, 'A2TEQQhb2W5mICczP')
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+    e.target.reset()
+  };
+
   return (
     <section class="contact">
       <div class="container">
@@ -27,21 +42,21 @@ function Contact() {
             <div class="right-side">
               <div class="topic-text">Let's Connect!</div>
               <p>If you have any questions or would like to reach out, please feel free to send me a message.</p>
-              <form action="https://formspree.io/f/xnqwlaaj" method="POST">
+              <form ref={form} onSubmit={sendEmail}>
                 <label class="input-box">
-                  <input type="text" name="name" placeholder="Enter your name"/>
+                  <input type="text" name="from_name" className='form-control' placeholder="Enter your name"/>
                 </label>
                 <label class="input-box">
-                  <input type="text" name="email" placeholder="Enter your email"/>
+                  <input type="email" name="from_email" className='form-control' placeholder="Enter your email"/>
                 </label>
                 <label class="input-box">
-                  <input type="text" name="subject" placeholder="Subject"/>
+                  <input type="text" name="subject" className='form-control' placeholder="Subject"/>
                 </label>
                 <label class="input-box message-box">
-                  <textarea class="form-control" rows="5" name="message" placeholder="Enter your message"></textarea>
+                  <textarea class="form-control" rows="5" name="message" className='form-control' placeholder="Enter your message"></textarea>
                 </label>
                 <div>
-                  <input class="button" type="button" value="Send Now" />
+                  <input class="button" type="submit" className='btn btn-info' value="Send Now" />
                 </div>
                 <div id="status" role="alert">
                 </div>
