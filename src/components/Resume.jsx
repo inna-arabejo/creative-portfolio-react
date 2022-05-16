7515a53f03189284ba998e448fdb342b67175986
Anne Saber <iarabejo3@gmail.com>
2022-05-15 21:26:05 -0500

    Created a download button to download the resume.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,9 +4,11 @@ import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 function Resume() {
+
   return(
     <div className='res' class="resume-pdf">
       <h1 className='resume-heading'>Resume</h1>
+      <a href={resume} target="_blank" rel="noopener noreferrer" className="btn btn-info btn-lg resume-btn">Download</a>
       <Document
       file={resume}
       onLoadError={console.error}
