55e357256fa659f02bcbc51caf6f8a7eb05736ac
Anne Saber <iarabejo3@gmail.com>
2022-05-15 22:42:38 -0500

    Added field validation for contact form.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import emailjs from 'emailjs-com';
 
 export const Contact = () => {
+  const initialValues = {from_name:"", from_email:"", subject:"", message:""}
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  }
 
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
 
     emailjs.sendForm('service_v8bo3k6', 'template_r58y7ci', form.current, 'A2TEQQhb2W5mICczP')
     .then((result) => {
@@ -15,6 +27,32 @@ export const Contact = () => {
     });
     e.target.reset()
   };
+
+  useEffect(() => {
+    console.log(formErrors)
+    if(Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors])
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.from_name) {
+      errors.from_name = "A name is required.";
+    }
+    if (!values.from_email) {
+      errors.from_email = "An email is required.";
+    } else if (!regex.test(values.from_email)) {
+      errors.from_email = "This is not a valid email format."
+    }
+    if (!values.subject) {
+      errors.subject = "A subject is required.";
+    }
+    if (!values.message) {
+      errors.message = "A message is required.";
+    }
+    return errors;
+  }
 
   return (
     <section class="contact">
@@ -44,22 +82,37 @@ export const Contact = () => {
               <p>If you have any questions or would like to reach out, please feel free to send me a message.</p>
               <form ref={form} onSubmit={sendEmail}>
                 <label class="input-box">
-                  <input type="text" name="from_name" className='form-control' placeholder="Enter your name"/>
+                  <input type="text" name="from_name" className='form-control' placeholder="Enter your name" value={formValues.from_name} onChange={handleChange}/>
+                  <p className='form-errors'>
+                    { formErrors.from_name }
+                  </p>
                 </label>
                 <label class="input-box">
-                  <input type="email" name="from_email" className='form-control' placeholder="Enter your email"/>
+                  <input type="email" name="from_email" className='form-control' placeholder="Enter your email" value={formValues.from_email} onChange={handleChange}/>
                 </label>
+                <p className='form-errors'>
+                  { formErrors.from_email }
+                </p>
                 <label class="input-box">
-                  <input type="text" name="subject" className='form-control' placeholder="Subject"/>
+                  <input type="text" name="subject" className='form-control' placeholder="Subject" value={formValues.subject} onChange={handleChange}/>
                 </label>
+                <p className='form-errors'>
+                  { formErrors.subject }
+                </p>
                 <label class="input-box message-box">
-                  <textarea class="form-control" rows="5" name="message" className='form-control' placeholder="Enter your message"></textarea>
+                  <textarea class="form-control" rows="5" name="message" className='form-control' placeholder="Enter your message" value={formValues.message} onChange={handleChange}></textarea>
                 </label>
+                <p className='form-errors'>
+                  { formErrors.message }
+                </p>
                 <div>
                   <input class="button" type="submit" className='btn btn-info' value="Send Now" />
                 </div>
-                <div id="status" role="alert">
-                </div>
+                {/* {Object.keys(formErrors).length === 0 && isSubmit ? (
+                <div className='ui message success'>Message Sent Successfully</div>) 
+                : (
+                <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+                )} */}
               </form>
           </div>
         </div>
